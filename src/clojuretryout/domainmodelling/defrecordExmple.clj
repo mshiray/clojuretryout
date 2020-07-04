5e565e26bc829eeffd740de12a0dc1af38f0fe51
Manish H <manishhiray@gmail.com>
2020-07-04 21:38:16 +0530

    domain data type creation with defrecord example

---
+++ src/clojuretryout/domainmodelling/defrecordExmple.clj
@@ -0,0 +1,24 @@
+;define namespace
+(ns com.clojure.domailmodel)
+
+;define enum type with keyword
+{:RETAIL 0 , :CORP 1}
+;defining composite data types as records
+;TODO: Move it to separate file
+(defrecord Address [street city state zip])
+(defrecord Customer [fname lname email phone type address])
+
+;create an instance of record
+(def terminator (Customer. "Arny" "T2" "AT2@skynet.com" 111110011 :CORP
+                  (Address. "Hasta La Vista Road" "LA" "CA" 345612)))
+
+;Define fucntion accepting the defined type record
+(defn printCustomer [customer]
+  (println (:fname customer) ""
+           (:lname customer) "#"
+           (:phone customer) "#"
+           (:type customer)
+           (-> customer :address :zip)) ; note the -> & placement of dto ref
+  )
+;invoke function with isntance of record type
+(printCustomer terminator)
