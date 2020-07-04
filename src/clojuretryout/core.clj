557582a05ef60e6ae943a733089532a9fbdbfa39
Manish H <manishhiray@gmail.com>
2020-07-04 13:28:33 +0530

    added sayHello function

--- src/clojuretryout/core.clj
+++ src/clojuretryout/core.clj
@@ -4,4 +4,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!" args))
+
+(defn sayHello [name]
+   (println "Hello " name)
+  )
