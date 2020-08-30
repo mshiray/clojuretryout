7f189571fec489021cdc99f0516679b22fe74709
mshiray <10815743+mshiray@users.noreply.github.com>
2020-08-30 13:48:23 +0530

    resolved wsl2 lein  poject files

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot clojuretryout.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
